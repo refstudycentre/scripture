61d68395b5b4c9a002a03d26a340e26a12b4a2b3
Gerhard Bijker (LT2015) <gerhard.bijker@ihs.com>
2016-01-02 17:04:21 +0200

    ScriptureBookSelector_AfterRefresh function updated (incomplete)

--- scripture.pickerstyle2.js
+++ scripture.pickerstyle2.js
@@ -46,23 +46,35 @@ function ScriptureSelectBook(tag) {
 
 function ScriptureBookSelector_AfterRefresh()
 {
+	//first we have to re-create te selector 
+	var rep = jQuery(".verse-picker-style-2 .scripture-book select")
+          .clone()
+          .wrap("<div></div>")
+          .parent().html()
+          .replace(/select/g,"ul")
+          .replace(/option/g,"li");
+
+	jQuery(".verse-picker-style-2 .scripture-book select").find(".scripture-book-selector").replaceWith(rep);
+		  
 	//remember the previously selected book (numeric - 1..66)
-	selectedBookNr = jQuery("#scripture-book select").val();
-	//console.log(selectedBookNr);
+	selectedBookNr = jQuery(".scripture-book select").val();
+	console.log(selectedBookNr);
 	
 	//find the new book name, for the newly selected translation
-	var found = jQuery("#scripture-book-selector li").filter(function(){
+	var found = jQuery(".scripture-book-selector li").filter(function(){
 	  //console.log(this.children[0].innerText == selectedBookNr);
 	  return (this.children[0].innerText == selectedBookNr);
 	});
-	//console.log(found);
+	console.log(found);
 	//set the book name based on the new translations book names
-	jQuery("scripture-book-alt").text(found[0].children[1].innerText);
-	jQuery(".scripture-book-selector li").click(function() {
-		jQuery(".scripture-book-selector").hide();
-		jQuery(".scripture-book-alt").text(this.children[1].innerText);
-		jQuery(".scripture-book select").val(this.children[0].innerText);
-	});
+	if (found.length > 0) {
+		jQuery("scripture-book-alt").text(found[0].children[1].innerText);
+		jQuery(".scripture-book-selector li").click(function() {
+			jQuery(".scripture-book-selector").hide();
+			jQuery(".scripture-book-alt").text(this.children[1].innerText);
+			jQuery(".scripture-book select").val(this.children[0].innerText);
+		});
+	}
 }
 
 
@@ -96,4 +108,7 @@ jQuery(document).ready(function(){
 	  	e.stopPropagation();
 	  }
     })	
+	
+	//first page-load:  refresh the book-selector menu
+	ScriptureBookSelector_AfterRefresh();
 });
